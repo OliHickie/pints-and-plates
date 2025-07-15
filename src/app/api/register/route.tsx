5bc328c9d87f1cf372a4b92130c99bebe9e5c92c
Oli Hickie <oli.h@hotmail.co.uk>
2025-07-15 17:30:46 +0100

    Handle errors on registration

--- src/app/api/register/route.tsx
+++ src/app/api/register/route.tsx
@@ -5,28 +5,40 @@ import bcrypt from 'bcrypt';
 const prisma = new PrismaClient();
 
 export async function POST(request: Request) {
-  const body = await request.json();
+  let body 
+  try {
+    body = await request.json();
+  } catch (error) {
+    return NextResponse.json({ error: 'Invalid JSON' }, { status: 400 });
+  }
+
   const { email, password, name } = body;
 
   if (!email || !password || !name) {
     return NextResponse.json({ error: 'Missing fields' }, { status: 400 });
   }
 
-  const existingUser = await prisma.user.findUnique({ where: { email } });
+  try {
 
-  if (existingUser) {
-    return NextResponse.json({ error: 'User already exists' }, { status: 400 });
+    const existingUser = await prisma.user.findUnique({ where: { email } });
+
+    if (existingUser) {
+      return NextResponse.json({ error: 'User already exists' }, { status: 409 });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await prisma.user.create({
+      data: {
+        email,
+        name,
+        hashedPassword,
+      },
+    });
+
+    return NextResponse.json({ success: true });
+  } catch (error) {
+    console.error('Error creating user:', error);
+    return NextResponse.json({ error: 'Internal server error' }, { status: 500 });
   }
-
-  const hashedPassword = await bcrypt.hash(password, 10);
-
-  await prisma.user.create({
-    data: {
-      email,
-      name,
-      hashedPassword,
-    },
-  });
-
-  return NextResponse.json({ success: true });
 }
